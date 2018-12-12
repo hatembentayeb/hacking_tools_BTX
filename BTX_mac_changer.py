b1ebe51aca782ffc4f5a2ed836d491ea215e152f
hatem ben tayeb <hatemtayeb2@gmail.com>
2018-12-12 18:54:18 +0100

    finel version of mac changer

--- BTX_mac_changer.py
+++ BTX_mac_changer.py
@@ -1,29 +1,81 @@
 #!/user/bin/env python
 
 import subprocess
-command = "ifconfig -a | sed \'s/[ \t].*//;/^$/d\'"  # the shell command
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+import optparse
+import re
+# command = "ifconfig -a | sed \'s/[ \t].*//;/^$/d\'"  # the shell command
+# process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+#
+#
+# output = process.communicate()
+# interfaces = output[0].decode('ascii')
+# xx= interfaces.split(':\n')
+# #print(xx[:-1])
+# print("[+] interfaces on your device :\n")
+# for i  in range(len(xx[:-1])):
+#     print('{} : {}'.format(i, xx[i]))
 
 
-output = process.communicate()
-interfaces = output[0].decode('ascii')
-xx= interfaces.split(':\n')
-#print(xx[:-1])
-print("[+] interfaces on your device :\n")
-for i  in range(len(xx[:-1])):
-    print('{} : {}'.format(i,xx[i]))
-
-interface = input("[*] choose your your interface :")
 
 
+def get_arguments():
+	parser = optparse.OptionParser()
+	parser.add_option("-i", "--interface", dest="interface", help="changing the mac for this interface")
+	parser.add_option("-m", "--mac", dest="new_mac", help="the mac address")
+	(options, arguments) = parser.parse_args()
 
-if interface in xx :
-    print("[+] changing mac address for {}".format(interface))
-    # subprocess.call("ifconfig wlp9s0 down ", shell=True)
-    # subprocess.call("ifconfig wlp9s0 hw ether 00:11:22:33:55 ", shell=True)
-    # subprocess.call("ifconfig wlp9s0 up ", shell=True)
+	if not options.interface :
+		print("[-] Please specify an interface , use --help for more Info")
+	elif not options.new_mac:
+		print("[-] Please specify a mac address , use --help for more Info")
+
+	return options
+
+
+
+def change_mac(interface, new_mac):
+		 print("[+] changing the mac address for {} to {}".format(interface,new_mac))
+		 subprocess.call(["ifconfig", interface, "down"])
+		 subprocess.call(["ifconfig", interface, "hw","ether",new_mac])
+		 subprocess.call(["ifconfig", interface, "up"])
+
+
+def get_current_mac(interface):
+	ifconfig_result = subprocess.check_output(["ifconfig", interface])
+	res = re.search(b'\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2}', ifconfig_result)
+	if res:
+		return res.group(0).decode('ascii')
+	else:
+		print("[-] Could not read the MAC arddess!")
+
+
+options= get_arguments()
+mac = get_current_mac(options.interface)
+print("current MAC  : {}".format( str(mac)))
+#change_mac(options.interface,options.new_mac)
+mac = get_current_mac(options.interface)
+if mac == options.new_mac:
+	print("[+] MAC address was sucsessfully changed to : {}".format(mac))
 else:
-    print("[-] wrong interface or you miss typing the interface ! check please :)")
+	print("[-] Mac address did not be changed!")
+
+
+
+
+
+
+
+
+
+# if interface in xx :
+#     print("[+] changing mac address for {}".format(interface))
+#     # subprocess.call("ifconfig {} down ".format(interface), shell=True)
+#     # subprocess.call("ifconfig {} hw ether  {}".format(interface,new_mac), shell=True)
+#     # subprocess.call("ifconfig {} up ".format(interface), shell=True)
+#
+
+# else:
+#     print("[-] wrong interface or you miss typing the interface ! check please :)")
 
 
 
--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-import subprocess
-command = "ifconfig -a | sed \'s/[ \t].*//;/^$/d\'"  # the shell command
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+import optparse
+parser = optparse.OptionParser()
 
-
-output = process.communicate()
-interfaces = output[0].decode('ascii')
-xx= interfaces.split(':\n')
-#print(xx[:-1])
-print("[+] interfaces on your device :\n")
-for i  in range(len(xx[:-1])):
-    print('{} : {}'.format(i,xx[i]))
-
-interface = input("[*] choose your your interface :")
-
-
-
-if(all([interface for i in xx])):
-    print("[+] this is your interface : {}".format(interface))
-else:
-    print("[-] wrong interface or you miss typing the interface ! check please :)")
+parser.add_option("-i", "--interface", dest="hatemen", help="changing the mac for this interface")
+parser.parse_args()
